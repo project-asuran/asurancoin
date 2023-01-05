dc0eae66b9e506260a4b1f897e06bc5f816d7efc
Sankar Srinivasan <petra.srini@gmail.com>
2023-01-05 21:25:57 +0530

    Update bitcoinunits.cpp

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,10 +42,10 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
+    case BTC: return QString("ASRN");
     case mBTC: return QString("lites");
     case uBTC: return QString("photons");
-    case SAT: return QString("liteoshi");
+    case SAT: return QString("sanoshi");
     default: return QString("???");
     }
 }
@@ -67,7 +67,7 @@ QString BitcoinUnits::description(int unit)
     case BTC: return QString("Litecoins");
     case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
     case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case SAT: return QString("Liteoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case SAT: return QString("Sanoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
